7da7fb027ab3b221fdbfef3ba22d8a0b73e01b20
Derek G <45305531+dGrowl@users.noreply.github.com>
2024-12-05 02:54:28 -0800

    Add dependency prettier

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders'
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -12,7 +12,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		scripts: z.array(z.string()).optional(),
 	}),
-});
+})
 
-export const collections = { blog };
+export const collections = { blog }
