0201a6e028ba936e206bbe9967363818790a1e85
Derek G <45305531+dGrowl@users.noreply.github.com>
2025-01-01 17:51:51 -0800

    Set metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = '/dev/dGrowl'
+export const SITE_DESCRIPTION =
+	'Developer musing on software, experiments, and technology via project notes and blog posts.'
